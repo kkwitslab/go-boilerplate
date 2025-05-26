8aba6e2f10c24de3f07ccee11ee3ba88e9cef5f4
Kabir Kalsi <kabir.kalsi@thewitslab.com>
2025-05-26 16:07:04 +0530

    feat: Implement gRPC Gateway

--- api/rest/server.go
+++ api/rest/server.go
@@ -1,12 +1,21 @@
 package rest
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net/http"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/adaptor"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/kkwitslab/go-boilerplate/api/rest/v1/middleware"
+	"github.com/kkwitslab/go-boilerplate/internal/config"
 	"github.com/kkwitslab/go-boilerplate/internal/di"
 	"github.com/kkwitslab/go-boilerplate/internal/models"
+	"github.com/kkwitslab/go-boilerplate/internal/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"gorm.io/gorm"
 )
 
@@ -26,5 +35,17 @@ func InitializeServer() (*fiber.App, error) {
 		return nil, fmt.Errorf("failed to invoke db dependency: %v", err)
 	}
 
+	app.All("/grpc/*", adaptor.HTTPHandler(createGrpcGatewayMux(context.Background())))
 	return app, nil
+}
+
+func createGrpcGatewayMux(ctx context.Context) http.Handler {
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+
+	err := pb.RegisterHelloWorldHandlerFromEndpoint(ctx, mux, config.AppConfig.GetGRPCListenAddress(), opts)
+	if err != nil {
+		log.Fatalf("Failed to register gRPC-Gateway handler: %v", err)
+	}
+	return mux
 }
--- go.mod
+++ go.mod
@@ -4,8 +4,10 @@ go 1.24
 
 require (
 	github.com/gofiber/fiber/v2 v2.52.6
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.26.3
 	github.com/joho/godotenv v1.5.1
 	go.uber.org/dig v1.18.1
+	google.golang.org/genproto/googleapis/api v0.0.0-20250303144028-a0af3efb3deb
 	google.golang.org/grpc v1.72.0
 	google.golang.org/protobuf v1.36.6
 	gorm.io/driver/postgres v1.5.11
@@ -20,7 +22,7 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	golang.org/x/sync v0.13.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20250218202821-56aae31c358a // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250303144028-a0af3efb3deb // indirect
 )
 
 require (
